6d0f2088e781820be76bca7abb52d7ef3570b69b
Matthew McNaughton <mamcnaughton@gmail.com>
2012-03-02 12:01:16 -0500

    Implemented PreferencesActivity. Created "home" menu for main activity.

--- src/jm/org/data/area/AreaActivity.java
+++ src/jm/org/data/area/AreaActivity.java
@@ -1,14 +1,72 @@
 package jm.org.data.area;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.actionbarcompat.ActionBarActivity;
 
 public class AreaActivity extends ActionBarActivity {
-    /** Called when the activity is first created. */
+	SharedPreferences preferences;
+	private String TAG = AreaActivity.class.getSimpleName();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main); 
+        setContentView(R.layout.main);
+        
+        //Initialize preferences
+        String idsAPIKey = getString(R.string.pref_idsKey);
+        String bingAPIKey = getString(R.string.pref_bingKey);
+        
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if ((preferences.getString(idsAPIKey, null) == null) || (preferences.getString(bingAPIKey, null) == null)){
+        	startActivity(new Intent(this, AreaPreferencesActivity.class));
+        	Toast.makeText(this, "Please setup preferences", Toast.LENGTH_LONG);
+        	return;
+        }
+        
+        Button btnPrefs = (Button) findViewById(R.id.btn_showPrefs);
+        btnPrefs.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String idsKey = preferences.getString("idsKey", "n/a");
+				String bingKey = preferences.getString("bingKey", "n/a");
+				String dateRange = preferences.getString("timePeriod", null);
+				showPrefs(idsKey, bingKey, dateRange);
+			}
+		});
+    }
+    
+    private void showPrefs(String idsKey, String bingKey, String dateRange) {
+		Toast.makeText(
+				AreaActivity.this,
+				String.format("IDS API Key: %s, Bing API Key: %s and Time period: %s years", idsKey,
+						bingKey, dateRange), Toast.LENGTH_LONG).show();
+	}
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater menuInflater = getMenuInflater();
+    	menuInflater.inflate(R.menu.home, menu);
+    	
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.menu_prefs:
+    		startActivity(new Intent(AreaActivity.this, AreaPreferencesActivity.class));
+    	}
+    	return super.onOptionsItemSelected(item);
     }
 }
---
+++ src/jm/org/data/area/AreaPreferencesActivity.java
@@ -0,0 +1,27 @@
+package jm.org.data.area;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+
+public class AreaPreferencesActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, AreaActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
