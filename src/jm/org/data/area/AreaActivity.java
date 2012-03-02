568ee9e8a24a8425b680d98e521d905ab8dfa96f
Matthew McNaughton <mamcnaughton@gmail.com>
2012-03-01 22:20:14 -0500

    Updated to work with Android SDKs because 3.0

--- src/jm/org/data/area/AreaActivity.java
+++ src/jm/org/data/area/AreaActivity.java
@@ -9,6 +9,6 @@ public class AreaActivity extends ActionBarActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main); 
     }
 }
