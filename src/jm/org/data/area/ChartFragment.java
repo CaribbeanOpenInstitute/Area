4e875dc74b23b9c8d0327d2c112d7b44e0368be0
Matthew McNaughton <mamcnaughton@gmail.com>
2012-03-07 15:08:56 -0500

    Updated Home activity with tabbed fragments outline for swiping between articles, reports and charts. Incorporated necessary, which will eventually be moved to indicators dashboard activity.

--- src/jm/org/data/area/ChartFragment.java
+++ src/jm/org/data/area/ChartFragment.java
@@ -23,6 +23,16 @@ public class ChartFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		if (container == null) {
+            // We have different layouts, and in one of them this
+            // fragment's containing frame doesn't exist.  The fragment
+            // may still be created from its saved state, but there is
+            // no reason to try to create its view hierarchy because it
+            // won't be displayed.  Note this is not needed -- we could
+            // just run the code below, where we would create and return
+            // the view hierarchy; it would just never be used.
+            return null;
+        }
 		View view = inflater.inflate(R.layout.chart_dashboard, container, false);
 		return view;
 	}
