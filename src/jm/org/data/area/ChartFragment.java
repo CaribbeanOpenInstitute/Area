1e837a977d567e3cb91831247f90bed8acaa4f30
Matthew McNaughton <mamcnaughton@gmail.com>
2012-03-08 13:50:12 -0500

    Moved previous "home" functions to the indicator dashboard (the correct place). Created respective Home activity and fragment. Updated activity with menu items.
    Outstanding:
    1. Need to implement Search Interface
    2. Need to implement List views on the Home fragment

--- src/jm/org/data/area/ChartFragment.java
+++ src/jm/org/data/area/ChartFragment.java
@@ -9,10 +9,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ChartFragment extends Fragment {
+	public static final String TAG = ChartFragment.class.getSimpleName();
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.e("TEST", "Hello");
+		Log.e(TAG, "Hello");
 	}
 	
 	@Override
@@ -23,7 +25,7 @@ public class ChartFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		if (container == null) {
+		/*if (container == null) {
             // We have different layouts, and in one of them this
             // fragment's containing frame doesn't exist.  The fragment
             // may still be created from its saved state, but there is
@@ -32,14 +34,14 @@ public class ChartFragment extends Fragment {
             // just run the code below, where we would create and return
             // the view hierarchy; it would just never be used.
             return null;
-        }
+        }*/
 		View view = inflater.inflate(R.layout.chart_dashboard, container, false);
 		return view;
 	}
 	
 	public void setText(String item) {
 		Log.d("ChartFragment", item);
-		TextView view = (TextView) getView().findViewById(R.id.detailsText);
+		TextView view = (TextView) getView().findViewById(R.id.chartText);
 		view.setText(item);
 	}
 
